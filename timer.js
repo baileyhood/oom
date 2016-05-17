5618f4e25183cced3ac0822160b4033c73aa1a71
baileyhood <bailey@livelihooddesignco.com>
2016-05-17 07:06:31 -0400

    changed timer, trying to get message to read out how long you meditated for

--- timer.js
+++ timer.js
@@ -1,46 +1,71 @@
-$(document).ready(function() {
-            function beginTimer(countRemaining) {
-                var totaltime = $("#time-input").val() || 60;
-                $('#time').html (countRemaining);
-                console.log("time", totaltime);
-                function update(percent) {
-                    var deg;
-                    if (percent < (totaltime / 2)) {
-                        deg = 90 + (360 * percent / totaltime);
-                        $('.pie').css('background-image',
-                            'linear-gradient(' + deg +
-                            'deg, transparent 50%, white 50%),linear-gradient(90deg, white 50%, transparent 50%)'
-                        );
-                    } else if (percent >= (totaltime / 2)) {
-                        deg = -90 + (360 * percent / totaltime);
-                        $('.pie').css('background-image',
-                            'linear-gradient(' + deg +
-                            'deg, transparent 50%, #1fbba6 50%),linear-gradient(90deg, white 50%, transparent 50%)'
-                        );
-                    }
-                }
-                var count = countRemaining || totaltime;
-                myCounter = setInterval(function() {
-                    count -= 1;
-                    $('#time').html(count);
-                    update(count);
-                    if (count == 0) clearInterval(
-                        myCounter);
-                }, 1000);
-            }
-            $("#stop-timer-button").on("click", function() {
-                event.preventDefault();
-                clearInterval(myCounter);
-            });
-            $('#start-timer-button').on('click', function() {
-                event.preventDefault();
-                beginTimer();
-                console.log();
-            })
-            $('#resume-timer-button').on('click', function () {
-              event.preventDefault();
-              var countRemaining = parseInt($('#time').text());
-              console.log('count =', countRemaining);
-              beginTimer(countRemaining);
-            })
-});
+var min = 25;
+var sec = 0;
+var timer;
+setInHTML();
+
+function add(){
+  min++;
+  setInHTML();
+  getSetTime();
+}
+function reduce(){
+  if (min > 0) { //can't go lower than 0
+    min--;
+    setInHTML();
+    getSetTime();
+  }
+}
+
+function start(){
+  document.getElementById('message').innerHTML = ""; //no message b/c timer is still running
+  if (min === 0 && sec === 0){ //adding message once timer is finished
+     document.getElementById('message').innerHTML = "Your session has finished! You meditated for " + timeMeditated + " minutes";
+    return;
+  }
+  if (sec === 0){ //reducing minute count once seconds reach 0 (i.e. one minute has passed)
+    --min;
+    sec = 59;
+  } else {
+    --sec;
+  }
+
+  setInHTML();
+
+  timer = setTimeout('start()',1000);
+}
+
+function pause(){
+  clearTimeout(timer);
+}
+
+function reset(){
+  min = 25;
+  sec = 0;
+  clearTimeout(timer);
+  setInHTML();
+  document.getElementById('message').innerHTML = "";
+}
+
+
+//adding text in DOM
+function setInHTML(){
+
+  if (min < 10){
+    document.getElementById('min').innerHTML = "0" + min;
+  } else {
+  document.getElementById('min').innerHTML = min;
+  }
+  if (sec < 10){
+    document.getElementById('sec').innerHTML = "0" + sec;
+  } else {
+    document.getElementById('sec').innerHTML = sec;
+  }
+}
+
+var timeMeditated = getSetTime();
+
+function getSetTime () {
+  var timeMeditated = document.getElementById('min').innerHTML;
+  return timeMeditated;
+  console.log("Set time: ", timeMeditated);
+}
