63c7d1a9d60c211bba51e8ce03cee8779848f5b0
baileyhood <bailey@livelihooddesignco.com>
2016-05-22 08:10:52 -0400

    made into object literal notation

--- timer.js
+++ timer.js
@@ -1,71 +1,71 @@
-var min = 25;
-var sec = 0;
-var timer;
-var timeMeditated = 0;
-setInHTML();
-getSetTime();
+$(document).ready(function() {
+  timer.init();
+});
 
-function add() {
-    min++;
-    setInHTML();
-    getSetTime();
-}
+var timer = {
+  min: 25,
+  sec: 0,
+  // timer: "",
+  timeMeditated: 0,
 
-function reduce() {
-    if (min > 0) { //can't go lower than 0
-        min--;
-        setInHTML();
-        getSetTime();
+  init: function () {
+    timer.setInHTML();
+    timer.getSetTime();
+  },
+  add: function () {
+    timer.min++;
+    timer.setInHTML();
+    timer.getSetTime();
+  },
+  reduce: function () {
+    if (timer.min > 0) { //can't go lower than 0
+        timer.min--;
+        timer.setInHTML();
+        timer.getSetTime();
     }
-}
-
-function start() {
-
+  },
+  start: function () {
     $('#message').html(""); //no message b/c timer is still running
-    if (min === 0 && sec === 0) { //adding message once timer is finished
+    if (timer.min === 0 && timer.sec === 0) { //adding message once timer is finished
         $('#message').html(
-            "Your session has finished! You meditated for " + timeMeditated +
+            "Your session has finished! You meditated for " + timer.timeMeditated +
             " minutes");
         return;
     }
-    if (sec === 0) { //reducing minute count once seconds reach 0 (i.e. one minute has passed)
-        --min;
-        sec = 59;
+    if (timer.sec === 0) { //reducing minute count once seconds reach 0 (i.e. one minute has passed)
+        --timer.min;
+        timer.sec = 59;
     } else {
-        --sec;
+        --timer.sec;
     }
-    setInHTML();
-    timer = setTimeout('start()', 1000);
-}
-
-function pause() {
-    clearTimeout(timer);
-}
-
-function reset() {
-        min = 25;
-        sec = 0;
-        clearTimeout(timer);
-        setInHTML();
-        $('#message').html ("");
-    }
-    //adding text in DOM
-
-function setInHTML() {
-    if (min < 10) {
-        $('#min').html("0" + min);
+    timer.setInHTML();
+    timer.timer = setTimeout('timer.start()', 1000);
+  },
+  pause: function () {
+    clearTimeout(timer.timer);
+  },
+  reset: function () {
+    timer.min = 25;
+    timer.sec = 0;
+    clearTimeout(timer.timer);
+    timer.setInHTML();
+    $('#message').html (""); //adding text in DOM
+  },
+  setInHTML: function () {
+    if (timer.min < 10) {
+        $('#min').html("0" + timer.min);
     } else {
-        $('#min').html(min);
+        $('#min').html(timer.min);
     }
-    if (sec < 10) {
-        $('#sec').html("0" + sec);
+    if (timer.sec < 10) {
+        $('#sec').html("0" + timer.sec);
     } else {
-        $('#sec').html(sec);
+        $('#sec').html(timer.sec);
     }
-}
-
-function getSetTime() {
-    timeMeditated = $('#min').html();
-    $('#session-length').html(timeMeditated + " minutes");
-    return timeMeditated;
+  },
+  getSetTime: function () {
+    timer.timeMeditated = $('#min').html();
+    $('#session-length').html(timer.timeMeditated + " minutes");
+    return timer.timeMeditated;
+  }
 }
